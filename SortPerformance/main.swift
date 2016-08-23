d1e6568ca18ea25d036e54ea78b931c1056362e9
Eugen Fedchenko <eugen.fedchenko@bluechipsys.com>
2016-08-23 22:42:45 +0300

    Initial Commit

---
+++ SortPerformance/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SortPerformance
+//
+//  Created by Eugen Fedchenko on 8/23/16.
+//  Copyright © 2016 Eugen Fedchenko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
